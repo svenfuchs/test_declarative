acf2b8bfc05c2507b7fd0a296e6db98b210c0907
Sven Fuchs <svenfuchs@artweb-design.de>
2010-07-21 23:58:55 +0200

    fix Module.test

--- lib/test_declarative.rb
+++ lib/test_declarative.rb
@@ -1,24 +1,20 @@
-module TestDeclarative
-  def self.included(base)
-    base.class_eval do
-      def self.test(name, &block)
-        test_name = "test_#{name.gsub(/\s+/,'_')}".to_sym
-        defined = instance_method(test_name) rescue false
-        raise "#{test_name} is already defined in #{self}" if defined
-        if block_given?
-          define_method(test_name, &block)
-        else
-          define_method(test_name) do
-            flunk "No implementation provided for #{name}"
-          end
+targets = [Module]
+targets << Test::Unit::TestCase     if defined?(Test::Unit::TestCase)
+targets << MiniTest::Unit::TestCase if defined?(MiniTest::Unit::TestCase)
+
+targets.each do |target|
+  target.class_eval do
+    def test(name, &block)
+      test_name = "test_#{name.gsub(/\s+/,'_')}".to_sym
+      defined = instance_method(test_name) rescue false
+      raise "#{test_name} is already defined in #{self}" if defined
+      if block_given?
+        define_method(test_name, &block)
+      else
+        define_method(test_name) do
+          flunk "No implementation provided for #{name}"
         end
       end
     end
   end
 end
-
-classes = [Module]
-classes << Test::Unit::TestCase     if defined?(Test::Unit::TestCase)
-classes << MiniTest::Unit::TestCase if defined?(MiniTest::Unit::TestCase)
-
-classes.each { |c| c.send(:include, TestDeclarative) }
