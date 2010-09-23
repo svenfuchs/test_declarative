2ffc6ff0b87e7b968887f1534d46fda908c01020
Krekoten' Marjan <krekoten@gmail.com>
2010-09-23 23:19:19 +0300

    Remove debug output

--- test/test_declarative_test.rb
+++ test/test_declarative_test.rb
@@ -11,7 +11,6 @@ class TestDeclarativeTest < Test::Unit::TestCase
   def test_adds_a_test_method
     called = false
     assert_nothing_raised { Test::Unit::TestCase.test('some test') { called = true } }
-    puts Test::Unit::TestCase.public_instance_methods(true).grep(/^test/).map { |m| m.to_s }
     Test::Unit::TestCase.new(:'test_some_test').run((RUBY_VERSION < '1.9.1' ? Test::Unit::TestResult : MiniTest::Unit).new) {}
     assert called
   end
